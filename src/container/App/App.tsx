fcbf9b8c1b804ed20ab672b32c94a1117267fcb6
Filipov <andrijfilipov73@gmail.com>
2023-03-10 17:15:43 +0200

    make counter

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,7 +1,44 @@
+import { useState } from 'react'
 import './App.scss'
 
 type Props = {}
 const App = (props: Props) => {
-    return <div className="hi">Hello start work</div>
+    const [firstCount, setFirstCount] = useState<number>(0)
+    const [secondCount, setSecondCount] = useState<number>(0)
+    const [thirdCount, setThirdCount] = useState<number>(0)
+
+    return (
+        <>
+            <div className="container">
+                <div className="main-cart">
+                    <h1 className="title">First task:</h1>
+                    <div className="box-button">
+                        <button
+                            className="button"
+                            onClick={() => setFirstCount(firstCount + 1)}
+                        >
+                            change count({firstCount})
+                        </button>
+                    </div>
+                    <div className="box-button">
+                        <button
+                            className="button"
+                            onClick={() => setSecondCount(secondCount + 1)}
+                        >
+                            change count({secondCount})
+                        </button>
+                    </div>
+                    <div className="box-button">
+                        <button
+                            className="button"
+                            onClick={() => setThirdCount(thirdCount + 1)}
+                        >
+                            change count({thirdCount})
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 export default App
